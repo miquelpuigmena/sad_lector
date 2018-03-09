1efbb87b3ba9daa0f1cd8610b6ce4f0167ab70b5
bertaserracanta <33903081+bertaserracanta@users.noreply.github.com>
2018-03-09 12:47:25 +0100

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,23 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
-/**
- *
- * @author lsadusr10
- */
 public class Constants {
     
-    static final int DRETA = 1000;
-    static final int ESQUERRA = 1001;
-    static final int INICI = 1002;
-    static final int FI = 1003;
-    static final int INSERT = 1004;
-    static final int SUPRIMIR = 1005;
-    static final int BACKSPACE = 1006;
+    static final int RIGHT = -1;
+    static final int LEFT = -2;
+    static final int HOME = -3;
+    static final int FIN = -4;
+    static final int INSERT = -5;
+    static final int SUPRIMIR = -6;
+    static final int BACKSPACE = -7;
     
 }
