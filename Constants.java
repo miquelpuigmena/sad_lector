a3ced882d5f1c9fa3e3a034440395157a820bea2
bertaserracanta <33903081+bertaserracanta@users.noreply.github.com>
2018-03-09 11:17:59 +0100

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -12,12 +12,12 @@
  */
 public class Constants {
     
-    static final int DRETA = 1000;
-    static final int ESQUERRA = 1001;
-    static final int INICI = 1002;
-    static final int FI = 1003;
-    static final int INSERT = 1004;
-    static final int SUPRIMIR = 1005;
-    static final int BACKSPACE = 1006;
+    static final int DRETA = -1;
+    static final int ESQUERRA = -1;
+    static final int INICI = -3;
+    static final int FI = -4;
+    static final int INSERT = -5;
+    static final int SUPRIMIR = -6;
+    static final int BACKSPACE = -7;
     
 }
